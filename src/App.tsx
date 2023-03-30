99d04e7129df4eea9c827c5b20a71d6c82bed293
MariannaPo <mapodmasko@gmail.com>
2023-03-30 18:36:02 +0300

    less3_props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Accordion from './components/Accordion/Accordion';
+import Rating from './components/Rating/Rating';
 
 function App() {
   return (
@@ -15,50 +17,4 @@ function App() {
 function AppTitle () {
   return <>my components</>
 }
-
-function Star () {
-  return (
-    <div>star</div>
-  )
-}
-
-function Rating () {
-  return (
-    <div>
-      <Star/>
-      <Star/>
-      <Star/>
-    </div>
-  )
-}
-
-function Accordion () {
-  return(
-    <div>
-      <AccordionTitle/>
-      <AccordionBody/>
-    </div>
-  )
-}
-
-function AccordionTitle () {
-  return(
-    <>
-    <h3>Menu</h3>
-    </>
-  )
-}
-
-function AccordionBody () {
-  return (
-    <>
-    <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-    </>
-  )
-}
-
 export default App;
---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Rating () {
+    return (
+      <div>
+        <Star/>
+        <Star/>
+        <Star/>
+      </div>
+    )
+  }
+  function Star () {
+    return (
+      <div>star</div>
+    )
+  }
+  export default Rating;
